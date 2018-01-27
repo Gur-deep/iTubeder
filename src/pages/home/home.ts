c148ff3176aae416fb05ba30886a31435e92adf9
Gurdeep Sing <gurdeepsingh,devgun@gmail.com>
2018-01-27 21:50:02 +0530

    Search Suggestions

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SearchDataProvider } from '../../providers/search-data/search-data';
 import { SearchResultsPage } from '../search-results/search-results';
